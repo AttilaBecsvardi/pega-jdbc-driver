9ed97ac1d1613c27f6661bc457435901e93a58bb
attilabecsvardi <attila.becsvardi@gmail.com>
2021-11-02 08:27:21 +0100

    improved error messages

---
+++ src/main/java/io/github/attilabecsvardi/pega/jdbc/restAPI/JDBCError.java
@@ -0,0 +1,34 @@
+package io.github.attilabecsvardi.pega.jdbc.restAPI;
+
+public class JDBCError {
+    private String errorMessage;
+    private String sqlState;
+    private String errorCode;
+
+    public JDBCError() {
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getSqlState() {
+        return sqlState;
+    }
+
+    public void setSqlState(String sqlState) {
+        this.sqlState = sqlState;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+}
